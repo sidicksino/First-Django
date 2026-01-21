0ffa4dbd5a0498ca011811138b8de8d14344e312
SidickSino <sidickabdoulayesino1@gmail.com>
2026-01-21 02:16:09 +0200

    add delete and update urls

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import fetch_news, create_news
+from .views import fetch_news, create_news, news
 
 urlpatterns = [
     path('fetch_news/', fetch_news, name='fetch_news'),
     path('create_news/', create_news, name='create_news'),
+    path('news/<int:pk>/', news, name='news'),
 ]
--- api/views.py
+++ api/views.py
@@ -18,3 +18,24 @@ def create_news(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def news(request, pk):
+    try:
+        news = News.objects.get(pk=pk)
+    except News.DoesNotExist:
+        return Response({'error': 'News not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        return Response(NewsSerializer(news).data)
+    
+    elif request.method == 'PUT':
+        serializer = NewsSerializer(news, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        news.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
