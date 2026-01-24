38f612f899a727a972224b639895bb16d7f034bb
SidickSino <sidickabdoulayesino1@gmail.com>
2026-01-25 01:02:03 +0200

    feat: Configure News model admin and add a build script for dependency installation, static file collection, and database migrations.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'publishedAt', 'url')
+    list_filter = ('publishedAt', 'category')
